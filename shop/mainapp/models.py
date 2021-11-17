a56c95bbf809f592972aca978103e1c3baf4d9a5
AnnTka <tovliani.mze@gmail.com>
2021-11-17 05:04:27 +0300

    change models.py (add 2 models for display dogs on the main page and order model), create 2 dogs through the admin, add views.py using a template base.html, add 1st url

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,17 +1,42 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.utils import timezone
 
 from django.contrib.auth import get_user_model
 # settings.AUTH_USER_MODEL
 User = get_user_model()
 
 
-# /categories/adult_dog or puppy == slug
+class LatestDogsManager:
+    # for display dogs on the main page
+    @staticmethod
+    def get_dog_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')  # prioritizing the output of results
+        dogs = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_dogs = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            dogs.extend(model_dogs)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        dogs, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                    )
+        return dogs
+
+
+class LatestDogs:
+
+    objects = LatestDogsManager()
+
+
 class Category(models.Model):
 
     name = models.CharField(max_length=255, verbose_name='Имя категории')
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True)  # /categories/adult_dog or puppy == slug
 
     def __str__(self):
         return self.name
@@ -19,7 +44,7 @@ class Category(models.Model):
 
 class Dog(models.Model):
 
-    # abstract model without migration
+    # abstract model without migration, parent class
     class Meta:
         abstract = True
 
@@ -30,7 +55,7 @@ class Dog(models.Model):
     breed = models.TextField(max_length=100, verbose_name='Порода')
     color = models.TextField(max_length=255, verbose_name='Окрас')
     gender = models.TextField(max_length=10, verbose_name='Пол')
-    age = models.CharField(max_length=2, verbose_name='Возраст')
+    age = models.TextField(max_length=10, verbose_name='Возраст')
     price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена', null=True)
 
     def __str__(self):
@@ -86,6 +111,46 @@ class Customer(models.Model):
 
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
     phone = models.CharField(max_length=15, verbose_name='Номер телефона')
+    orders = models.ManyToManyField('Order', verbose_name='Заказы покупателя', related_name='related_customer')
 
     def __str__(self):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
+
+
+class Order(models.Model):
+
+    STATUS_NEW = 'new'
+    STATUS_IN_PROGRESS = 'in_progress'
+    STATUS_READY = 'is_ready'
+    STATUS_COMPLETED = 'completed'
+
+    BUYING_TYPE_SELF = 'self'
+    BUYING_TYPE_DELIVERY = 'delivery'
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'Новый заказ'),
+        (STATUS_IN_PROGRESS, 'Заказ в обработке'),
+        (STATUS_READY, 'Заказ готов'),
+        (STATUS_COMPLETED, 'Заказ выполнен')
+    )
+
+    BUYING_TYPE_CHOICES = (
+        (BUYING_TYPE_SELF, 'Самовывоз'),
+        (BUYING_TYPE_DELIVERY, 'Доставка')
+    )
+
+    customer = models.ForeignKey(Customer, verbose_name='Покупатель', related_name='related_orders', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15,  verbose_name='Номер телефона')
+    address = models.CharField(max_length=1024, verbose_name='Адрес', null=True, blank=True)
+    status = models.CharField(
+        max_length=25, verbose_name='Статус заказа', choices=STATUS_CHOICES, default=STATUS_NEW
+    )
+    buying_type = models.CharField(
+        max_length=25, verbose_name='Тип заказа', choices=BUYING_TYPE_CHOICES, default=BUYING_TYPE_SELF
+    )
+    comment = models.TextField(verbose_name='Комментарий к заказу', null=True, blank=True)
+    created_at = models.DateTimeField(auto_created=True, verbose_name='Дата создания товара')
+    order_date = models.DateField(verbose_name='Дата получения заказа', default=timezone.now)
+
+    def __str__(self):
+        return str(self.id)
