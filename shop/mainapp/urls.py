22fb1af80329e1d9193a2e1bcce3a1f5a366826e
AnnTka <tovliani.mze@gmail.com>
2021-11-20 01:46:23 +0300

    add logout n registration, return RegistrationForm (-_-)

--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import BaseView, DogDetailView, CategoryDetailView, LoginView
+from .views import BaseView, DogDetailView, CategoryDetailView, LoginView, RegistrationView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
     path('dogs/<str:slug>/', DogDetailView.as_view(), name='dog_detail'),
     # http://127.0.0.1:8000/dogs/our/Adel/ - result: Адель
     path('category/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page="/"), name='logout'),
+    path('registration/', RegistrationView.as_view(), name='registration')
 ]
